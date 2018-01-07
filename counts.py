09828bf360d0cc0a563f27980f20549d32ad8ab9
Juncheng Li <30983433+leejoonsung007@users.noreply.github.com>
2018-01-07 22:43:28 +0000

    Update counts.py

--- counts.py
+++ counts.py
@@ -3,6 +3,7 @@ from page_parsing import url_list
 from page_parsing import item_info
 
 while True:
-    print('The number of items information',item_info.find().count())
+    # count the number of url_list and item_info
     print('The number of url list',url_list.find().count())
+    print('The number of items information',item_info.find().count())
     time.sleep(10)
